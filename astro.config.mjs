749f75891777a87997ecc58d68d7f6447d9b94eb
Carl-Thomas Ledmyr <Carl-Thomas.Ledmyr@newsec.com>
2026-03-02 20:12:31 +0100

    Use Node.js 20 for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'hybrid',
-  adapter: vercel(),
+  adapter: vercel({ nodeVersion: '20' }),
   site: process.env.SITE_URL,
 });
